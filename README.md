7edd2b016c57b8297cc00996bf3a940584875d71
CHIH-CHAN <s010064@shsh.tw>
2019-06-14 17:24:48 +0800

    Version 1

--- README.md
+++ README.md
@@ -1,82 +1,61 @@
-# Graph-Theory-Project-Template
-Project template of course - Graph Theory (EE6622E) in National Cheng Kung University.
+# 2019-spring-gt
 
-## How to run
+## 如何執行
 
 ### Linux
 
 ```
 # clone first (with submodule)
-$ git clone --recursive https://github.com/<your-name>/Graph-Theory-Project-Template.git
-# (Option) build libfakemn.a
-$ cd fake-mininet && make lib && cd .. && cp fake-mininet/libfakemn.a .
+$ git clone --recursive https://github.com/CHIH-CHAN/Graph-Theory-Project-Template.git
 # build
 $ make 
 # run 
 $ ./main.out
 ```
+### Input file
+輸入檔名為"topo.txt"，須放在目錄底下 \
+輸入格式範例如下:\
+a b\
+b c \
+則此檔中有a b c三個vertex，且ａ<->b b<->c
 
-### Windows
-
-If your PC is `window 10`, then you need to install the related dependencies. For example, if you are using `VSCode` as your IDE, then you can install the plugin of `C/C++` in your vscode. And then install the following programs/packages:
-* mingw
-* git
-
-Step by step to create `c_cpp_properties.json` and `tasks.json`:
-* `Ctrl+Shift+P` -> `C/C++: Edit Configuration` -> then you have the first JSON file.
-* `Ctrl+Shift+P` -> `Tasks: Configure Task` -> then you have the second JSON file.
-
-Here is the setting of `c_cpp_properties.json` and `tasks.json`:
-```json
-# c_cpp_properties.json (If your mingw installed in another folder, then you have to change the value in `compilterPath`)
-{
-    "configurations": [
-        {
-            "name": "MinGW",
-            "intelliSenseMode": "gcc-x64",
-            "includePath": [
-                "$(workspaceFolder)"
-            ],
-            "compilerPath": "C:/mingw/bin/gcc.exe",
-            "cStandard": "c11",
-            "cppStandard": "c++11"
-        }
-    ],
-    "version": 4
-}
-
-# tasks.json
-{
-    "version": "2.0.0",
-    "tasks": [
-        {
-            "label": "build",
-            "type": "shell",
-            "command": "g++",
-            "args": [
-                "-g",
-                "main.cc",
-                "-o",
-                "main.exe",
-                "-Ifake-mininet/lib",
-                "-g3",
-                "-L.",
-                "-lfakemn_win",
-                "-std=c++11"
-            ],
-            "group": {
-                "kind": "build",
-                "isDefault": true
-            }
-        }
-    ]
-}
+#### Default Input file (Example)
 ```
+a b
+b c
+c d
+d e
+e f
+f g
+g h
+h i
+i j
+b d
+c e
+f h
+g i
+```
+### Output file
+程式執行後，會自動產生以下檔案並放在目錄底下 
+1. output.txt\
+![Alt text](/pic/output.PNG)
+2. plot.dot
+![Alt text](/pic/plot.PNG)
 
-After all the settings have been done, we can press `Ctrl+Shift+B` to run the task, if your settings is correct, then there will have an executable file which named `main.exe`.
 
-Or you can just [DOWNLOAD a pre-built VM images](http://gofile.me/39GpL/XU5tznyO6) provided by TA.
+## 程式功能：
+解決中國郵差問題，並將路徑輸出到"output.txt"。
 
-> [Guide of environment setting](https://hackmd.io/-5WZQC-1QqOeV3KUX65tEw?view) on Windows.
+## 程式限制：
+只適用於無向圖、端點最多只能10個、端點之間的連線最多一條。
 
-## TODO - Your descriptions about solutions/algorithms/results
+## 解題邏輯：
+首先先使用Fake-mininet的API將輸入檔案讀進來轉換成拓樸並使用Gplot繪圖，然後將其轉換成adjacency matrix。\
+計算各個點的連接後找出其degree為奇數的點，利用Single-Pair Shortest Path來找到配對後最少的重複路徑，\
+再將這些路徑加入adjacency matrix之中，使此圖成為一尤拉圖，\
+最後利用Depth-First Search來進行尋找路徑的動作，並將這個路徑輸出到output.txt。
+
+## 參考連結：
+https://www.geeksforgeeks.org/dijkstras-shortest-path-with-minimum-edges/ \
+http://alrightchiu.github.io/SecondRound/shortest-pathintrojian-jie.html \
+http://alrightchiu.github.io/SecondRound/graph-depth-first-searchdfsshen-du-you-xian-sou-xun.html
--- main.cc
+++ main.cc
@@ -1,19 +1,304 @@
-/* 
-    Your main program goes here
-*/
 #include <iostream>
 #include "network_manager.h"
 #include "gplot.h"
 #include "path.h"
+#include <queue>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
 // create NetworkManager first
 NetworkManager *nm = new NetworkManager();
+string names[10];
+int adjacency[10][10]={0};
+int degree[10]={0},odd_connect[10]={0},odd_connect_temp[10]={0};
+int MinDistance(int*, int*); 
+vector<string> outpath;
+int ok=0;
+
+int Dijkstra(int start, int end, int path[]) ;
+int MinDistance(int* dist, int* Blackened) ;
+void Record(int* parent, int _d, int path[], int c) ;
+void DFS (int start, int length, int visit[], string names[], int size);
+void adjacency_print();
+int find_shortest(int odd[10],int oddnum,int oddnum_total);
+void degree_update();
+
+
+
 
 int main(int argc, char** argv){
 
-    /* start your program */
+    //input file and print
+	nm->interpret("topo.txt");
+	nm->print_all_e();
+    nm->print_all_v();
+	ofstream fout;
 
+	//make a adjacency matrix and count degree
+	Vertex *list =  nm->get_all_nodes();
+	Edge *edge = nm->elist;
+
+    while(list!=NULL){
+		names[nm->switch_num] = list->name;
+       	cout << "name : " << nm->switch_num << " = " << names[nm->switch_num] << endl;
+		list=list->next;
+		nm->switch_num = nm->switch_num + 1 ;
+	}
+	cout << "switch_num : "<< nm->switch_num << endl;
+	for(int i=0;i<10;i++)
+	{
+		for(int j=0;j<10;j++)
+		{
+			if(i>=nm->switch_num||j>=nm->switch_num)
+				adjacency[i][j] = 0 ;
+			else if(!nm->connected(names[i],names[j]) )
+				adjacency[i][j] = 1 ;
+		}
+	}
+	degree_update();
+	//print adjacency and degree
+	adjacency_print();
+	int path_test[10],odd[10],oddnum=0;
+	for(int i=0;i<10;i++) 
+		odd[i]=99;
+	//print odd degree vertex
+	for(int i=0;i<10;i++) 
+		if(degree[i]%2==1)
+		{
+			odd[oddnum] = i ;
+			cout<<"odd["<<oddnum<<"]="<<odd[oddnum]<<endl;
+			oddnum++;
+		}
+	//print odd vertex matching and repeat path length
+	cout<<endl<<"shortest_path:"<<find_shortest(odd,oddnum,oddnum)<<endl;
+	cout<<"odd vertex matching:"<<endl;
+	for(int i=0;i<oddnum;i++)
+	{
+		if(i%2==0)
+			cout<<odd_connect[i];
+		else
+			cout<<"->"<< odd_connect[i]<<endl;
+	}
+	//add repeat path to adjacency matrix
+	for(int i=0;i<10;i=i+2)
+	{
+		if(odd_connect[i]==odd_connect[i+1])
+			break;
+		for(int k=0;k<10;k++)
+			path_test[k]=-1;
+		Dijkstra(odd[i],odd[i+1],path_test);
+		for(int j=1;j<10;j++)
+		{
+			if(path_test[j]!=(-1))
+			{
+				adjacency[path_test[j]][path_test[j-1]] ++;
+				adjacency[path_test[j-1]][path_test[j]] ++;
+			}
+			else
+			{
+				adjacency[odd[i]][path_test[j-1]] ++;
+				adjacency[path_test[j-1]][odd[i]] ++;
+				break;
+			}		
+		}
+	}
+	degree_update();
+	adjacency_print();
+	int pathlength_final=0,visit[10]={0};
+	//count the path length and visit number
+	for(int i=0;i<10;i++)
+	{
+		pathlength_final+= degree[i]/2;
+		visit[i]= degree[i]/2;
+		if(i==0)
+			visit[0]++;
+	}
+	//use DFS to find path
+	DFS(0,pathlength_final,visit,names,nm->switch_num);
+	fout.open("output.txt");
+	fout<<"Final Path :\n"<< names[0]<<"->";
+	for(int i=0;i<outpath.size();i++)
+	{
+		if(i!=outpath.size()-1)
+			fout<<outpath[i]<<"->";
+		else
+			fout<<outpath[i];
+	}
+	fout<<endl;
+	fout.close();
+	//Gplot
+	Gplot *gp = new Gplot();
+    gp->gp_add(nm->elist);
+    gp->gp_dump(true);
+    gp->gp_export("plot");
+
+	
     return 0;
+}
+
+
+
+int Dijkstra(int start, int end, int path[]) 
+{ 
+	int min_distance; 
+	int dist[10],parent[10]; 
+	int Blackened[10]={0},pathlength[10]={0}; 
+
+	parent[start] = -1; 
+
+	for (int i = 0; i < 10; i++) 
+		dist[i] = 99; 
+
+	dist[start] = 0; 
+	for (int i = 0; i < 9; i++) { 
+		min_distance = MinDistance(dist, Blackened); 
+		if (min_distance == 99) 
+			break; 
+		else 
+		{ 
+			Blackened[min_distance] = 1; 
+			for (int j = 0; j < 10; j++) 
+			{ 
+				if (!Blackened[j] && adjacency[min_distance][j] 
+					&& dist[min_distance] + adjacency[min_distance][j] < dist[j]) 
+				{ 
+					parent[j] = min_distance; 
+					pathlength[j] = pathlength[parent[j]] + 1; 
+					dist[j] = dist[min_distance] + adjacency[min_distance][j]; 
+				} 
+				else if (!Blackened[j] && adjacency[min_distance][j] 
+						&& dist[min_distance] + adjacency[min_distance][j] == dist[j] 
+						&& pathlength[min_distance] + 1 < pathlength[j]) 
+				{ 
+					parent[j] = min_distance; 
+					pathlength[j] = pathlength[min_distance] + 1; 
+				} 
+			} 
+		} 
+	} 
+	if (dist[end] != 99) 
+		Record(parent, end, path, -1); 
+	else
+		return 99;
+	return	dist[end];
+			
+} 
+
+int MinDistance(int* dist, int* Blackened) 
+{ 
+	int min = 99, min_index, v; 
+	for (v = 0; v < 10; v++) 
+		if (!Blackened[v] && dist[v] < min) 
+		{ 
+			min = dist[v]; 
+			min_index = v; 
+		} 
+	return (min == 99) ? 99 : min_index; 
+} 
+
+void Record(int* parent, int _d, int path[], int c) 
+{ 
+	if (parent[_d] == -1)
+		return; 
+	c++;
+	Record(parent, parent[_d], path, c); 	
+	path[c] = _d;
+
+}
+
+void adjacency_print()
+{
+	cout<<"  ";
+	for(int i=0;i<nm->switch_num;i++)
+		cout<<names[i]<<" ";
+	
+	cout<<"degree"<<endl;
+	for(int i=0;i<nm->switch_num;i++)
+	{
+		cout<<names[i]<<" ";
+		for(int j=0;j<nm->switch_num;j++)
+			cout<<adjacency[i][j]<<" ";
+		cout<<degree[i]<<endl;
+	}	
+}
+
+int find_shortest(int odd[10],int oddnum,int oddnum_total)
+{
+	int path_test[10]={0};
+	if(oddnum==2)
+	{
+		odd_connect[0]=odd[0];
+		odd_connect[1]=odd[1];
+
+		return Dijkstra(odd[0],odd[1],path_test);
+	}
+	else
+	{
+		int new_odd[10]={0},shortest=999,temp,shortest_odd[10];
+		for(int i=1;i<oddnum;i++)
+		{
+			for(int j=1,index=0;index<oddnum-2;j++)
+			{
+				if(i!=j)
+				{
+					new_odd[index]=odd[j];
+					index++;
+				}
+			}
+			temp = 	Dijkstra(odd[0],odd[i],path_test) +
+					find_shortest(new_odd,oddnum-2,oddnum_total);
+			if(temp<shortest)
+			{
+				shortest = temp ;
+				odd_connect_temp[oddnum-2]=odd[0];
+				odd_connect_temp[oddnum-1]=odd[i];
+				for(int k=0;k<10;k++)
+					shortest_odd[k]=new_odd[k];
+			}
+		}
+		odd_connect[oddnum-2]=odd_connect_temp[oddnum-2];
+		odd_connect[oddnum-1]=odd_connect_temp[oddnum-1];
+		find_shortest(shortest_odd,oddnum-2,oddnum_total-2);
+
+		return shortest;
+	}
+}
+void degree_update()
+{
+	for(int i=0;i<10;i++)
+	{
+		degree[i]=0;
+		for(int j=0;j<10;j++)
+			degree[i]+=adjacency[i][j];
+	}
+}
+void DFS (int start, int length, int visit[], string names[], int size)
+{
+	visit[start] = visit[start] - 1 ;
+	for(int i = 0; i < size; i++)
+	{
+		if (adjacency[start][i] != 0 && visit[i] != 0)
+		{
+			adjacency[start][i]--;
+			adjacency[i][start]--;
+			outpath.push_back(names[i]);
+			length--;			
+			if (length > 0)
+				DFS(i, length, visit, names, size);
+			else if (i == 0)
+			{
+				ok = 1;
+				return;
+			}
+			if (ok == 1)  
+				return;
+			length++;
+			visit[i]++;
+			outpath.pop_back();
+			adjacency[start][i]++;
+			adjacency[i][start]++;
+		}	
+	}
 }
